b3cce40b7a4f3af4b547a78061076e21cdf096ea
FARES ADAM <farex64@gmail.com>
2025-09-09 21:50:43 +0400

    Create taskSlice.js

---
+++ frontend/src/store/taskSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  tasks: [],
+  loading: false,
+  error: null,
+};
+
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    fetchTasksStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchTasksSuccess: (state, action) => {
+      state.loading = false;
+      state.tasks = action.payload;
+    },
+    fetchTasksFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { fetchTasksStart, fetchTasksSuccess, fetchTasksFailure } = taskSlice.actions;
+export default taskSlice.reducer;
