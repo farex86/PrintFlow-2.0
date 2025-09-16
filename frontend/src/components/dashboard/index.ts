dcd006f3b636a444b2b74b13b9e03fa25f6409d5
FARES ADAM <farex64@gmail.com>
2025-09-16 11:51:03 +0400

    Delete frontend/src directory

--- frontend/src/components/dashboard/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as KPICard } from './KPICard';
-export { default as RecentActivity } from './RecentActivity';
-export { default as QuickActions } from './QuickActions';
--- frontend/src/hooks/index.js
+++
@@ -1,7 +0,0 @@
-// Custom hooks will be added here
-// Example: useAuth, useLocalStorage, useWebSocket, etc.
-
-export const useExample = () => {
-  // Custom hook implementation
-  return {};
-};
